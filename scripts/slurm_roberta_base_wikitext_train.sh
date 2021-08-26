fe9117e025d311552d6fdb66d4401192ea0726c4
zhujinguo <lechatelia@stu.xjtu.edu.cn>
2021-08-26 21:59:10 +0800

    samll change

--- scripts/slurm_roberta_base_wikitext_train.sh
+++ scripts/slurm_roberta_base_wikitext_train.sh
@@ -56,7 +56,7 @@ spring.submit arun --mpi=None  --job-name=${JOB_NAME} -n$GPUS --gpu   \
     --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
     --batch-size $MAX_SENTENCES --update-freq $UPDATE_FREQ \
     --max-update $TOTAL_UPDATES --log-format simple --log-interval 1 --save-dir $WORK_DIR/checkpoints \
-     --distributed-port ${PORT} --distributed-world-size $GPUS $PY_ARGS $PY_ARGS \
+     --distributed-port ${PORT} --distributed-world-size $GPUS  $PY_ARGS \
     2>&1 | tee -a $WORK_DIR/exp_$now.txt "
 
 elif [ $a == '198-8' ]; then
@@ -72,7 +72,7 @@ srun --partition=vc_research_2 --mpi=pmi2 \
     --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
     --batch-size $MAX_SENTENCES --update-freq $UPDATE_FREQ \
     --max-update $TOTAL_UPDATES --log-format simple --log-interval 1 --save-dir $WORK_DIR/checkpoints \
-     --distributed-port ${PORT} --distributed-world-size $GPUS $PY_ARGS $PY_ARGS \
+     --distributed-port ${PORT} --distributed-world-size $GPUS  $PY_ARGS \
     2>&1 | tee -a $WORK_DIR/exp_$now.txt
 else
   echo only SH1986 and SH1988 supported now 
