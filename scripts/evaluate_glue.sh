94909116e6d96e6a9c98920a5b8a3c49d71d2504
zhujinguo <lechatelia@stu.xjtu.edu.cn>
2021-08-28 13:24:52 +0800

    add evaluate on server

--- scripts/evaluate_glue.sh
+++ scripts/evaluate_glue.sh
@@ -3,6 +3,7 @@
 if [ $# -ge 2 ]; then
   TASKS=${1} # roberta_base roberta_large
   checkpoints=${2} # pretrained_checkpoints/roberta.large/model.pt
+  SRUN=${3:-'srun'}
 else
 echo sh scripts/evaluate_glue.sh ALL workdirs/slurm_roberta_large_glue/finetune
 fi
@@ -10,7 +11,14 @@ fi
 now=$(date +"%Y%m%d_%H%M%S")
 a=$(echo $HOSTNAME | cut  -c12-16)
 
-if [ $a == '198-6' -o "${SRUN}" == "spring" ]; then
+if [ "${SRUN}" == "server" ]; then
+
+
+python scripts/evaluate_glue.py \
+--checkpoints ${checkpoints} --tasks $TASKS \
+2>&1 | tee -a $checkpoints/glue_eval_$now.txt 
+
+elif [ $a == '198-6' -o "${SRUN}" == "spring" ]; then
 
 spring.submit arun --mpi=None  --job-name=glue-evaluate -n1 --gpu   \
 --gres=gpu:1 --ntasks-per-node=1 --cpus-per-task 4 \
